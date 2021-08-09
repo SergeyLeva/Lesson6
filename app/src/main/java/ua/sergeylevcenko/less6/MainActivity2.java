870edc56bdb7479423a93c458133e2ca46646765
Sergey Leva <levchenkox@gmail.com>
2021-08-09 18:58:17 +0300

    test

--- app/src/main/java/ua/sergeylevcenko/less6/MainActivity2.java
+++ app/src/main/java/ua/sergeylevcenko/less6/MainActivity2.java
@@ -1,10 +1,12 @@
 package ua.sergeylevcenko.less6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -28,6 +30,24 @@ public class MainActivity2 extends AppCompatActivity {
         menu.add(2, 2, 2, "третья ");
 
         return super.onCreateOptionsMenu(menu);
+       }
+        @Override
+        public boolean onOptionsItemSelected (@NonNull MenuItem item){
+            if (item.getItemId() == 0) {
+                Intent intentOne = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intentOne);
+            }
+            if (item.getItemId() == 1) {
+                Intent intentSecond = new Intent(MainActivity2.this, MainActivity2.class);
+                startActivity(intentSecond);
+            }
+            if (item.getItemId() == 2) {
+                Intent intentThree = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intentThree);
+            }
 
+            return super.onOptionsItemSelected(item);
+        }
     }
-}
+
+
--- app/src/main/java/ua/sergeylevcenko/less6/MainActivity3.java
+++ app/src/main/java/ua/sergeylevcenko/less6/MainActivity3.java
@@ -1,10 +1,12 @@
 package ua.sergeylevcenko.less6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
@@ -15,13 +17,10 @@ public class MainActivity3 extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
     }
 
-
     public void onClick(View view) {
         Intent intent3 = new Intent(MainActivity3.this, MainActivity.class);
         startActivity(intent3);
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -31,4 +30,24 @@ public class MainActivity3 extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == 0) {
+            Intent intentOne = new Intent(MainActivity3.this, MainActivity.class);
+            startActivity(intentOne);
+        }
+        if (item.getItemId() == 1) {
+            Intent intentSecond = new Intent(MainActivity3.this, MainActivity2.class);
+            startActivity(intentSecond);
+        }
+        if (item.getItemId() == 2) {
+            Intent intentThree = new Intent(MainActivity3.this, MainActivity3.class);
+            startActivity(intentThree);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
